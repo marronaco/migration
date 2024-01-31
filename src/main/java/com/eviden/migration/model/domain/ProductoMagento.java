6c5d4cc54fc162aee7752c3c8008dddeb28e79e9
PalmerinErwerle <franifjrf@gmail.com>
2024-01-31 09:11:19 +0100

    Creacion entidad ProductoMagento

--- src/main/java/com/eviden/migration/model/domain/ProductoMagento.java
+++ src/main/java/com/eviden/migration/model/domain/ProductoMagento.java
@@ -12,9 +12,26 @@ import lombok.*;
 @Table(name = "productomagento")
 public class ProductoMagento implements Serializable {
     
-    @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="productomagento_sequence")
-    @SequenceGenerator(name="productomagento_sequence", sequenceName="productomagento_sequence", allocationSize=100)
-    private Long id;
+    private String sku; // Palabra mediante a la que acceder al producto (suele ser igual al nombre).
+    private String name;
+    private int attribute_set_id;
+    private float price;
+    private int status;
+    private int visibility; // Valor que dictamina quien puede ver el dato (1-4).
+    private String type_id;
+    private String created_at;
+    private String updated_at;
+    private String weight;
+
+    private int qty; // Cantidad de productos en stock.
+
+    private String file; // Archivo de imagen en Base64.
+    //private String image_value; // Direccion en  memoria fisica del servidor donde esta la imagen.
+
+    private String short_description;
+
+    private String country_of_manufacturer; // Pais de creacion (2 letras).
+
+
     
 }
