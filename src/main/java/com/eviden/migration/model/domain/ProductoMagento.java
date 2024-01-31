4332ce387c8b909df2bc4efb9332f6ab423221ce
PalmerinErwerle <franifjrf@gmail.com>
2024-01-31 10:00:41 +0100

    Update entidad ProductoMagento

--- src/main/java/com/eviden/migration/model/domain/ProductoMagento.java
+++ src/main/java/com/eviden/migration/model/domain/ProductoMagento.java
@@ -14,7 +14,7 @@ public class ProductoMagento implements Serializable {
     
     private String sku; // Palabra mediante a la que acceder al producto (suele ser igual al nombre).
     private String name;
-    private int attribute_set_id; // ??
+    private String url_key; // URL para acceder al producto (deberia ser que el SKU).
 
     private float price; // Precio de venta del producto.
     private float cost; // Precio de compra del producto (lo que nos costó).
@@ -23,20 +23,19 @@ public class ProductoMagento implements Serializable {
     private int visibility; // Valor que dictamina quien puede ver el dato (1-4).
     private String type_id; // ??
 
-    private String created_at;
-    private String updated_at;
-
     private String weight;
-    //private String width;
-    //private String height;
+
+    private String width;
+    private String height;
 
     private int qty; // Cantidad de productos en stock.
-    private boolean stock_status; // Si está o no a la venta.
+    private boolean is_in_stock; // Si está o no a la venta.
 
     private String base64_image; // Archivo de imagen en Base64.
     private String image_value; // Direccion en  memoria fisica del servidor donde esta la imagen.
 
-    private String short_description;
+    private String description; // Descripcion larguita.
+    private String short_description; // Descripcion cortita.
 
     private String country_of_manufacturer; // Pais de creacion (2 letras).
 
