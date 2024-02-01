5ae3c7432b581295637bddf9867e94d7d722e48e
TitoMunozAbreu <munoztitojose@gmail.com>
2024-02-01 09:33:38 +0100

    actualizacion de atributos y anotaciones

--- src/main/java/com/eviden/migration/model/request/HttpRequestMagentoProduct.java
+++ src/main/java/com/eviden/migration/model/request/HttpRequestMagentoProduct.java
@@ -2,9 +2,13 @@ package com.eviden.migration.model.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HttpRequestMagentoProduct {
 
@@ -13,31 +17,39 @@ public class HttpRequestMagentoProduct {
     private Product product;
 
     //Inner clases de los atributos de tipo objeto
+    @Data
+    @Builder
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Product {
         @JsonProperty("sku")
         private String sku;
+
         @JsonProperty("name")
         private String name;
+
         @JsonProperty("attribute_set_id")
         private int attribute_set_id;
+
         @JsonProperty("price")
         private double price;
-        @JsonProperty("status")
-        private int status;
-        @JsonProperty("visibility")
-        private int visibility;
+
         @JsonProperty("type_id")
         private String type_id;
+
         @JsonProperty("weight")
         private double weight;
+
         @JsonProperty("custom_attributes")
         private List<Custom_attributes> customAttributes;
     }
 
+    @Data
+    @Builder
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Custom_attributes {
         @JsonProperty("attribute_code")
-        private String attribute_code;
+        private String attributeCode;
         @JsonProperty("value")
-        private Object value;
+        private String value;
     }
 }
