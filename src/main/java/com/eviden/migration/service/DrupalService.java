201aee67795db9eeaec66c7c8815ecb84f697cf9
TitoMunozAbreu <munoztitojose@gmail.com>
2024-02-02 11:55:00 +0100

    gestion de acceso a los endpoints de Drupal

--- src/main/java/com/eviden/migration/service/DrupalService.java
+++ src/main/java/com/eviden/migration/service/DrupalService.java
@@ -1,10 +1,79 @@
 package com.eviden.migration.service;
 
+import com.eviden.migration.exceptions.AuthenticationFailedException;
+import com.eviden.migration.exceptions.ResourceNotFoundException;
+import com.eviden.migration.model.response.DrupalAuthCsrf;
+import com.eviden.migration.model.response.DrupalProducto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import javax.transaction.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
+@Slf4j
 @Service
-@Transactional
 public class DrupalService {
+    private final WebClient drupalWebClient;
+
+    public DrupalService(WebClient drupalWebClient) {
+        this.drupalWebClient = drupalWebClient;
+    }
+
+    /**
+     * Metodo para obtner el token de CSRF
+     * @return token CSRF
+     */
+    public Mono<DrupalAuthCsrf> obtenerDrupalCSRF(){
+        log.info("Drupal: iniciando autenticacion CSRF...");
+        //obtner el CSRF de drupal
+        return drupalWebClient.post()
+                .uri(uriBuilder -> uriBuilder.path("/user/token.json").build())
+                .header("Content-Type", "application/json")
+                .retrieve()
+                .bodyToMono(DrupalAuthCsrf.class)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    //manejo detallado de la excepcion
+                    HttpStatus status = ex.getStatusCode();
+                    String response = ex.getResponseBodyAsString();
+                    return Mono.error(
+                            new AuthenticationFailedException("Error: '%s' | Mensaje: '%s?"
+                                    .formatted(status,response)));
+                })
+                .doOnError(error -> {
+                    log.error("Error: '%s'".formatted(error));
+                });
+    }
+
+    /**
+     * Metodo que obtiene un detalle del producto segun NID
+     * @param nid
+     * @return producto
+     */
+    public Mono<DrupalProducto> obtenerProductoByNid(Integer nid){
+        log.info("Drupal: obteniendo producto del NID '{}'", nid);
+        return drupalWebClient.get()
+                .uri("/node/{nid}",nid)
+                .header("Content-Type","application/json")
+                .retrieve()
+                .bodyToMono(DrupalProducto.class)
+                .onErrorResume(WebClientResponseException.class, ex -> {
+                    //manejo detallado de la excepcion
+                    HttpStatus status = ex.getStatusCode();
+                    String response = ex.getResponseBodyAsString();
+                    return Mono.error(
+                            new ResourceNotFoundException("Error: '%s' | Mensaje: '%s?"
+                                    .formatted(status,response)));
+                })
+                .doOnError(error -> {
+                    log.error("Error: '%s'".formatted(error));
+                });
+    }
+
+
+
     
 }
