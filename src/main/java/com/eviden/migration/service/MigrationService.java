6bd582950ee6db073615662d87b153bab6e7440d
TitoMunozAbreu <munoztitojose@gmail.com>
2024-02-07 11:38:56 +0100

    subcribirse a cada llamada que devuelve un webClient

--- src/main/java/com/eviden/migration/service/MigrationService.java
+++ src/main/java/com/eviden/migration/service/MigrationService.java
@@ -1,10 +1,10 @@
 package com.eviden.migration.service;
 
-import com.eviden.migration.model.response.DrupalAuthCsrf;
+import com.eviden.migration.model.request.MagentoAuthToken;
 import com.eviden.migration.model.response.DrupalProducto;
+import com.eviden.migration.model.response.MagentoProductResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -19,37 +19,29 @@ public class MigrationService {
     }
 
     //migrar producto de drupal a magento
-    public Mono<Void> migracionProducto(){
-        //obtener producto en drupal
-        Mono<DrupalAuthCsrf> drupalCSRF = drupalService.obtenerDrupalCSRF()
+    public void migracionProducto(){
+        //magento: autenticar usuario
+        Mono<MagentoAuthToken> authMagento = magentoService.autenticarUsuario();
+        //subscribir a la llamada
+        authMagento.subscribe();
+
+        //drupal:obtener producto en drupal
+        Mono<DrupalProducto> drupalProducto = drupalService.obtenerDrupalCSRF()
                 .doOnSuccess(authCsrf -> {
                     log.info("CSRF: {}", authCsrf.getToken());
                 })
-                .doOnError(error -> {
-                    // Manejar errores, imprimir en el log o realizar acciones específicas
-                    log.error("Error during migration: {}", error.getMessage(), error);
+                .then(drupalService.obtenerProductoByNid(2)
+                        .doOnSuccess(producto -> {
+                            log.info("Drupal Producto {}", producto.toString());
+                        })
+                );
+        //magento: insertar producto
+        Mono<MagentoProductResponse> magentoResponse = magentoService.insertarProducto(drupalProducto.block())
+                .doOnSuccess(magentoProducto -> {
+                    log.info("Magento Producto {}", magentoProducto.toString());
                 });
-        Mono<DrupalProducto> drupalProducto = drupalService.obtenerProductoByNid(2)
-                .doOnSuccess(producto -> {
-                    log.info("Producto {}", producto);
-                })
-                .doOnError(error -> {
-                    log.error("Error during migration: {}", error.getMessage(), error);
-                });
-
-        drupalCSRF.subscribe();
-        drupalProducto.subscribe();
-
-        return drupalProducto.then();
-//        //autenticar usuario en magento
-//        Mono<Void> magentoAuthMono = magentoService.autenticarUsuario();
-//
-//        return Mono.zip(drupalProductoMono, magentoAuthMono)
-//                .flatMap(tuple -> {
-//                    DrupalProducto drupalProducto = tuple.getT1();
-//
-//                    return magentoService.insertarProducto(drupalProducto);
-//                })
-//                .then();
+        //subscribir a la llamada
+        magentoResponse.subscribe();
+        
     }
 }
