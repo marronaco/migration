17625920d0c0e400ab6e9d7732bcdd6da43721b7
TitoMunozAbreu <munoztitojose@gmail.com>
2024-02-07 11:34:31 +0100

    actualizacion del atributo username

--- src/main/java/com/eviden/migration/model/request/MagentoAuthRequest.java
+++ src/main/java/com/eviden/migration/model/request/MagentoAuthRequest.java
@@ -11,7 +11,7 @@ import lombok.*;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MagentoAuthRequest {
     @JsonProperty("username")
-    private String userName;
+    private String username;
     @JsonProperty("password")
     private String password;
 }
